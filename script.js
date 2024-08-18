24b8cbc884d1aeb966b719a6d8014a7b3c7198eb
Sivdan777 <31362154+Sivdan777@users.noreply.github.com>
2024-08-17 23:50:46 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const mobileMenu = document.querySelector('nav.mobile-menu')
 const bodyEl = document.querySelector('body:not(.mobile-menu__left)')
 const topBar = document.querySelector('.topBar')
 const topBar__closeBtn = document.querySelector('.topBar__close-btn')
+const popularProductsLink = document.querySelector('#popular-products__link')
+const popularProducts = document.querySelector('#popular-products')
+const popularCategoriesLink = document.querySelector('#popular-categories__link')
+const popularCategories = document.querySelector('#popular-categories')
+const popularStoresLink = document.querySelector('#popular-stores__link')
+const popularStores = document.querySelector('#popular-stores')
 
 const openMobileMenu = () => {
   mobileMenu.classList.remove('hide')
@@ -30,7 +36,33 @@ topBar__closeBtn.addEventListener('click', () => {
 //   alert('hello')
 // })
 
-window.scrollTo({
-  top: 0,
-  behavior: "smooth",
-});
+popularCategories.style.display = 'none'
+popularStores.style.display = 'none'
+
+popularProductsLink.addEventListener('click', (e) => {
+  popularProducts.style.display = 'grid'
+  e.target.classList.add('underlined')
+  popularCategoriesLink.children[0].classList.remove('underlined')
+  popularStoresLink.children[0].classList.remove('underlined')
+  popularCategories.style.display = 'none'
+  popularStores.style.display = 'none'
+})
+
+popularCategoriesLink.addEventListener('click', (e) => {
+  popularCategories.style.display = 'grid'
+  e.target.classList.add('underlined')
+  popularProductsLink.children[0].classList.remove('underlined')
+  popularStoresLink.children[0].classList.remove('underlined')
+  popularProducts.style.display = 'none'
+  popularStores.style.display = 'none'
+})
+
+popularStoresLink.addEventListener('click', (e) => {
+  popularStores.style.display = 'grid'
+  e.target.classList.add('underlined')
+  popularProductsLink.children[0].classList.remove('underlined')
+  popularCategoriesLink.children[0].classList.remove('underlined')
+  popularProducts.style.display = 'none'
+  popularCategories.style.display = 'none'
+})
+
